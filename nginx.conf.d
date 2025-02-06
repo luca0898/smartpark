ae85d3914ab86733d0091564a241311821824b5b
luca0898 <lucaguiraldello@hotmail.com>
2025-02-06 13:43:28 -0300

    wip: Adiciona configuração do nginx e prometheus no docker

--- nginx.conf.d
+++ nginx.conf.d
@@ -21,3 +21,19 @@ server {
         proxy_set_header X-Forwarded-Proto $scheme;
     }
 }
+
+server {
+    listen 443 ssl;
+    server_name api.smartpark.local;
+
+    # ssl_certificate /etc/letsencrypt/live/api.smartpark.local/fullchain.pem;
+    # ssl_certificate_key /etc/letsencrypt/live/api.smartpark.local/privkey.pem;
+
+    location / {
+        proxy_pass http://api:5000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
